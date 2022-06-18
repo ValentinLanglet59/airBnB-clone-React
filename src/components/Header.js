622381313e0ff04ddef5f233436773fda734f810
ValentinLanglet59 <valdu59960@hotmail.fr>
2022-06-18 18:01:22 +0200

    header done

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+
+function Header() {
+    return (
+        <div className="header--background">
+            <div className="header--flex">
+                <i class="fa-solid fa-earth-americas"></i>
+                <h3>My travel journal!</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Header
